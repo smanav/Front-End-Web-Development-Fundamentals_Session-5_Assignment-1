0791d99dd8ce7accab2eeb6b54ecbf62718ad402
smanav <smanav1991@gmail.com>
2017-04-18 16:02:21 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+                  //Here is the script example
+
+                   var 
+                        Name="Messy", 
+                        Age=35, 
+                        DateOFBirth="23-4-1982", 
+                        PlaceOfBirth="Argentina";
+
+                    console.log(Name,Age,DateOFBirth,PlaceOfBirth);
